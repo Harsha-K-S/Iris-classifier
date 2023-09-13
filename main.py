39d24b68241cbabbd71d22b0c9d82675b3910bc4
Harsha-K-S <144898573+Harsha-K-S@users.noreply.github.com>
2023-09-13 15:06:10 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request, redirect, url_for
+import pickle
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('userInput.html')
+
+@app.route('/results/<string:cls>')
+def results(cls):
+    return render_template('results.html', cls=cls)
+
+@app.route('/submit', methods=['POST', 'GET'])
+def submit():
+    cls = ""
+    with open('iris_predict_model.pkl', 'rb') as file:
+        clsf = pickle.load(file)
+    if request.method == 'POST':
+        seplen = float(request.form['sepal-len'])
+        sepwid = float(request.form['sepal-wid'])
+        petallen = float(request.form['petal-len'])
+        petalwid = float(request.form['petal-wid'])
+        pre = clsf.predict([[seplen, sepwid, petallen, petalwid]])
+        if pre == 1:
+            cls = "Versicolor"
+        elif pre == 2:
+            cls = "virginica"
+        else:
+            cls = "setosa"
+    return redirect(url_for('results', cls=cls))
+
+if __name__ == '__main__':
+    app.run()
+
+
